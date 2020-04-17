45bc2f42a09bba4912672decdb156258ff99a0c2
adrianmunt <adrian_b_munt@yahoo.com>
2020-04-17 11:08:12 -0400

    First commit

---
+++ src/build/precache-manifest.0883a30e62f830fa00815565102abc54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dbc68eed7d17a3799f45",
+    "url": "/static/js/main.f749b66a.chunk.js"
+  },
+  {
+    "revision": "c04a4fbd02fb8f166d3c",
+    "url": "/static/js/2.227a7a13.chunk.js"
+  },
+  {
+    "revision": "dbc68eed7d17a3799f45",
+    "url": "/static/css/main.a3530d8b.chunk.css"
+  },
+  {
+    "revision": "4c2dc13123f74b504362ec9d04f5188f",
+    "url": "/index.html"
+  }
+];
