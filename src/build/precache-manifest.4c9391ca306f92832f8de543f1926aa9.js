69d652a9d4783f1232d22c8b4ea4a7bf075e5cc4
adrianmunt <adrian_b_munt@yahoo.com>
2020-04-21 10:06:51 -0400

    NiceBloke 1

---
+++ src/build/precache-manifest.4c9391ca306f92832f8de543f1926aa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b1e99f6dabc2ba40a65a9403f6a9d7a",
+    "url": "/static/media/paper.8b1e99f6.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "882a06f1d4916a399488",
+    "url": "/static/js/main.56e9d065.chunk.js"
+  },
+  {
+    "revision": "0aae8331acdd271102e9",
+    "url": "/static/js/2.f1b34484.chunk.js"
+  },
+  {
+    "revision": "882a06f1d4916a399488",
+    "url": "/static/css/main.760a10ec.chunk.css"
+  },
+  {
+    "revision": "d22b8af86555fedee86b189b6077755b",
+    "url": "/index.html"
+  }
+];
--- src/build/service-worker.js
+++ src/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0883a30e62f830fa00815565102abc54.js"
+  "/precache-manifest.4c9391ca306f92832f8de543f1926aa9.js"
 );
 
 workbox.clientsClaim();
